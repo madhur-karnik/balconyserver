c441901931d09fdb3576c863f39844285f210faa
Madhur Karnik <madhur.karnik@gmail.com>
2017-11-01 22:15:23 +0530

    Add node modules and modify app js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var helmet = require("helmet");
+var cors = require("cors");
+var errorHandler = require("errorhandler");
 var app;
 
 app = module.exports = express();
 app.set('trust proxy',1);
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(errorHandler());
+app.use(cors());
 
 app.get('/',function(req, res) {
     res.send("Hello World");
